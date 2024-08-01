413de6eb45ac025469a397386156941451f7d79c
Anshuman Joshi <joshianshuman0@gmail.com>
2024-08-01 22:40:45 +0530

    Some changes

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ import sys
 import numpy as np
 import pandas as pd
 import dill
+import pickle
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score
 
@@ -33,5 +34,13 @@ def evaluate_models(X_train,y_train,X_test,y_test,models):
             report[list(models.keys())[i]] = test_model_score
 
         return report
+    except Exception as e:
+        raise CustomException(e,sys)
+    
+
+def load_object(file_path):
+    try:
+        with open(file_path,"rb")as file_obj:
+            return pickle.load(file_obj)
     except Exception as e:
         raise CustomException(e,sys)
